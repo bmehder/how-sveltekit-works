e7f90c507d63da8449c078a7953b1ef3bf14057e
bmehder <bmehder@gmail.com>
2023-05-25 13:50:32 +0200

    latest

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,7 +2,7 @@
   import '$lib/app.css'
   import Header from '$lib/Header.svelte'
   import Footer from '$lib/Footer.svelte'
-  import BackToTop from '../lib/BackToTop.svelte'
+  import BackToTop from '$lib/BackToTop.svelte'
 </script>
 
 <div class="wrapper">
@@ -26,8 +26,7 @@
   main {
     width: 100%;
     max-width: var(--max-width);
-    padding-block: calc(var(--size) * 4);
-    padding-inline: calc(var(--size) * 2);
+    padding: calc(var(--size) * 3);
     margin-inline: auto;
     line-height: 1.5;
   }
